c849277a58ca77018a54f985531dd081629916de
J041 <46340016+J041@users.noreply.github.com>
2023-03-08 01:17:06 +0800

    Create about_us.php

---
+++ INF1005Project/about_us.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <?php
+        include "header.inc.php";
+        ?>
+    </head>
+    <body>
+        <?php
+        include "nav.inc.php";
+        ?>
+        <div class="container">
+            <div class="row">
+                <img src="/static/assets/img/logo.png" class="img-fluid" alt="Responsive image">
+                <div class="col-md-6 mx-auto my-5">
+                    <h2>About Us</h2>
+                    <p>We are a company that specializes in providing high-quality services and products to our customers. With years of experience and a team of dedicated professionals, we strive to meet the needs and exceed the expectations of our clients.</p>
+                    <p>Our mission is to deliver exceptional value and customer satisfaction through innovative solutions and superior customer service.</p>
+                    <p>Thank you for considering us for your needs. We look forward to working with you!</p>
+                </div>
+            </div>
+        </div>
+        <?php
+        include "footer.inc.php";
+        ?>
+    </body>
+</html>
