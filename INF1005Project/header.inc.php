81bbfc8203b8709482bf3e484509ae05fbf1291b
J041 <46340016+J041@users.noreply.github.com>
2023-03-08 00:33:57 +0800

    Create header.inc.php

---
+++ INF1005Project/header.inc.php
@@ -0,0 +1,29 @@
+<head>
+    <!<!-- CSS -->
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+          integrity=
+          "sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="static/assets/fontawesome/css/all.css"/>
+    <link rel="stylesheet" href="static/assets/css/main.css">
+
+    <!--jQuery-->
+    <script defer
+            src="https://code.jquery.com/jquery-3.4.1.min.js"
+            integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+            crossorigin="anonymous">
+    </script>
+    <!--Bootstrap JS-->
+    <script defer
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"
+            integrity="sha384-6khuMg9gaYr5AxOqhkVIODVIvm9ynTT5J4V1cfthmT+emCG6yVmEZsRHdxlotUnm"
+            crossorigin="anonymous">
+    </script>
+
+    <script defer src="static/assets/js/main.js"></script>
+
+    <title>Value Convenience Store</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
