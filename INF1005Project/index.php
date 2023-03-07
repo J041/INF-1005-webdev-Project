35d5ebeccb366a8981a6df331a9083274fb442c0
J041 <46340016+J041@users.noreply.github.com>
2023-03-08 00:34:11 +0800

    Create index.php

---
+++ INF1005Project/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <?php
+        include "header.inc.php";
+        ?>
+    </head>
+    <body>
+        <?php
+        include "nav.inc.php";
+        ?>
+        <div class="container">
+            <div class="row">
+                <h1>Welcome to Value Convenience Store</h1>
+            </div>
+        </div>
+        <?php
+        include "footer.inc.php";
+        ?>
+    </body>
+</html>
