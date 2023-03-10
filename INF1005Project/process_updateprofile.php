67a72cb8d9794de93ef9c46cbe6f74b9ef004715
J041 <46340016+J041@users.noreply.github.com>
2023-03-10 09:13:51 +0800

    Update process_updateprofile.php

--- INF1005Project/process_updateprofile.php
+++ INF1005Project/process_updateprofile.php
@@ -5,10 +5,13 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
 -->
 
 <?php
+
+require "function.php";
+
 /*
 * Helper function to authenticate the login.
 */
-function authenticateUser()
+function UpdateUser()
 {
     global $fname, $lname, $email, $pwd, $profilepic, $errorMsg, $success;
     // Create database connection.
@@ -26,10 +29,10 @@ function authenticateUser()
         
         // my code
         
-        if (!empty($_POST["email"]))
+        if (!empty($_POST["username"]))
         {
-            $stmt = $conn->prepare("UPDATE users SET email=? where user_id = ?");
-            $stmt->bind_param("s", $_POST["email"]);
+            $stmt = $conn->prepare("UPDATE Users SET username=? where email = ?");
+            $stmt->bind_param("ss", sanitize_input($_POST["username"]), 'customer3@gmail.com');
             $stmt->execute();
             $result = $stmt->get_result();
  
@@ -37,49 +40,47 @@ function authenticateUser()
         
         if (!empty($_POST["pwd"]))
         {
-            $stmt = $conn->prepare("UPDATE users SET password=? where user_id = ?");
-            $stmt->bind_param("s", $_POST["pwd"]);
+            $stmt = $conn->prepare("UPDATE users SET password=? where email = ?");
+            $stmt->bind_param("ss", password_hash($_POST["pwd"], PASSWORD_DEFAULT), 'customer3@gmail.com');
             $stmt->execute();
             $result = $stmt->get_result();
         } 
+        
+        
                 
-        if (!empty($_POST["profilepic"]))
-        {
-            $stmt = $conn->prepare("UPDATE users SET profile_img=? where user_id = ?");
-            $stmt->bind_param("s", $_POST["profilepic"]);
-            $stmt->execute();
-            $result = $stmt->get_result();
-        } 
+//        if (!empty($_POST["profilepic"]))
+//        {
+//            $stmt = $conn->prepare("UPDATE users SET profile_img=? where email = ?");
+//            $stmt->bind_param("s", $_POST["profilepic"]);
+//            $stmt->execute();
+//            $result = $stmt->get_result();
+//        } 
 
         
-        // Prepare the statement:
-        $stmt = $conn->prepare("SELECT * FROM world_of_pets_members WHERE email=?");
-        // Bind & execute the query statement:
-        $stmt->bind_param("s", $_POST["email"]);
-        $stmt->execute();
-        $result = $stmt->get_result();
+
         if ($result->num_rows > 0)
         {
+            $success = true;
             // Note that email field is unique, so should only have
             // one row in the result set.
-            $row = $result->fetch_assoc();
-            $fname = $row["fname"];
-            $lname = $row["lname"];
-            $pwd_hashed = $row["password"];
-            // Check if the password matches:
-//            $user_input_pwd_hashed = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
-//            if (!password_verify($user_input_pwd_hashed, $pwd_hashed))
-            $user_input_password = $_POST["pwd"];
-            if (!password_verify($user_input_password, $pwd_hashed))
-            {
-                // Don't be too specific with the error message - hackers don't
-                // need to know which one they got right or wrong. :)
-                $errorMsg = "Email not found or password doesn't match...";
-                $success = false;
-            }
-            else {
-                $success = true;
-            }
+//            $row = $result->fetch_assoc();
+//            $fname = $row["fname"];
+//            $lname = $row["lname"];
+//            $pwd_hashed = $row["password"];
+//            // Check if the password matches:
+////            $user_input_pwd_hashed = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
+////            if (!password_verify($user_input_pwd_hashed, $pwd_hashed))
+//            $user_input_password = $_POST["pwd"];
+//            if (!password_verify($user_input_password, $pwd_hashed))
+//            {
+//                // Don't be too specific with the error message - hackers don't
+//                // need to know which one they got right or wrong. :)
+//                $errorMsg = "Email not found or password doesn't match...";
+//                $success = false;
+//            }
+//            else {
+//                $success = true;
+//            }
         }
         else
         {
@@ -91,11 +92,10 @@ function authenticateUser()
     $conn->close();
 }
 
-authenticateUser();
+UpdateUser();
 
 if ($success){
-    $output = "<h3>Login successful!</h4>" .
-              "<h4>Welcome back, " . $fname . $lname . ".</h4>" .
+    $output = "<h3>Update successful!</h4>" .
               "<a href='index.php'><button class='btn btn-success' type='submit'>Return to Home</button></a>";
 } else {
     $output = "<h3>Oops!</h4>" .
