2eef811194235ba08f0248b3c46fcc87cb784655
ninjazenia <shawnoon123@gmail.com>
2023-03-14 03:18:43 +0800

    fixed login function

--- INF1005Project/login.php
+++ INF1005Project/login.php
@@ -8,6 +8,58 @@
     include "nav.inc.php";
     $usernameErr = $passwordErr = "";
     ?>
+    <?php
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+//        if (isset($_GET['message'])) {
+//            $message = $_GET['message'];
+//            echo $message;
+//        }
+    // display form data on submission if no errors
+    if (isset($_POST['submit'])) {
+        $echo = "test0";
+        authenticateUser2();
+    }
+
+    function authenticateUser2() {
+        $echo = "test 01";
+        $config = parse_ini_file('../private/db-config.ini');
+        $conn = new mysqli($config['servername'], $config['username'],
+                $config['password'], $config['dbname']);
+
+// Check connection
+        if ($conn->connect_error) {
+            $errorMsg = "Connection failed: " . $conn->connect_error;
+            $success = false;
+        } else {
+// Prepare the statement:
+            $stmt = $conn->prepare("SELECT * FROM Users WHERE username=? or email = ?");
+// Bind & execute the query statement:
+            $stmt->bind_param("ss", $_POST["username"], $_POST["username"]);
+
+            $stmt->execute();
+            echo "test case2";
+            $result = $stmt->get_result();
+            if ($result->num_rows > 0) {
+                $row = $result->fetch_assoc();
+                $username = $row["username"];
+                $pwd_hashed = $row["password"];
+                echo $pwd_hashed . " gapspace " . $_POST["pwd"];
+                header("Location: http://35.212.159.197/index.php");
+            } else {
+                echo "<h4>The following input errors were detected:</h4>";
+                echo "<p>" . $errorMsg . "</p>";
+                echo "<a href='http://35.212.159.197/register.php'><button>return to sign up</button></a>";
+            }
+            $stmt->close();
+
+            $conn->close();
+        }
+    }
+
+ 
+    ?>
+
     <body>
 
         <main class="container">
@@ -20,7 +72,7 @@
                 <div class="form-group">
                     <label for="fname">Username:</label>
                     <input class="form-control"type="text" id="username"
-                           name="username" maxlength="45" placeholder="Enter username"value="<?php echo $username; ?>">
+                           name="username" maxlength="45" placeholder="Enter username or email"value="<?php echo $username; ?>">
                     <span class="error"><?php if (isset($usernameErr)) echo $usernameErr; ?></span>
                 </div>
                 <div class="form-group">
@@ -31,81 +83,10 @@
                 </div>
 
                 <div class="form-group">
-                    <button class="btn btn-primary" type="submit">Submit</button>
+                    <button class="btn btn-primary" name = "submit" type="submit">Submit</button>
                 </div>
             </form>
         </main>
-        <?php
-        ini_set('display_errors', 1);
-        error_reporting(E_ALL);
-//        if (isset($_GET['message'])) {
-//            $message = $_GET['message'];
-//            echo $message;
-//        }
-        // display form data on submission if no errors
-        if (isset($_POST['submit'])) {
-            $echo = "test0";
-            authenticateUser();
-        };
-
-        function authenticateUser() {
-// Create database connection.
-            $echo = "test 01";
-            $config = parse_ini_file('../private/db-config.ini');
-            $conn = new mysqli($config['servername'], $config['username'],
-                    $config['password'], $config['dbname']);
-            echo $config['servername'];
-// Check connection
-            if ($conn->connect_error) {
-                $errorMsg = "Connection failed: " . $conn->connect_error;
-                $success = false;
-            } else {
-// Prepare the statement:
-                $stmt = $conn->prepare("SELECT * FROM Users WHERE username=?");
-// Bind & execute the query statement:
-                $stmt->bind_param("s", $_POST["username"]);
-
-                $stmt->execute();
-                echo "test case2";
-                $result = $stmt->get_result();
-                if ($result->num_rows > 0) {
-// Note that email field is unique, so should only have
-// one row in the result set.
-                    echo "test case3";
-                    $row = $result->fetch_assoc();
-                    $username = $row["username"];
-                    $pwd_hashed = $row["password"];
-
-                    echo $pwd_hashed . " gapspace " . $_POST["pwd"];
-                    if (password_verify($_POST["pwd"], $pwd_hashed)) {
-                        session_start();
-
-// regenerate the session ID every 30 minutes
-                        if (isset($_SESSION['last_activity']) && time() - $_SESSION['last_activity'] > 1800) {
-                            session_regenerate_id(true); // generate a new session ID
-                            $_SESSION['last_activity'] = time(); // update the last activity time
-                        }
-
-// set a session timeout of 24 hours
-                        session_set_cookie_params(86400);
-
-// set a session variable
-                        $_SESSION['username'] = $username;
-                        header("Location: http://35.212.159.197/index.php");
-                    } else {
-                        echo "<h4>The following input errors were detected:</h4>";
-                        echo "<p>" . $errorMsg . "</p>";
-                        echo "<a href='http://35.212.159.197/register.php'><button>return to sign up</button></a>";
-                    }
-                } else {
-                    $errorMsg = "Email not found or password doesn't match...";
-                    $success = false;
-                }
-                $stmt->close();
-            }
-            $conn->close();
-        }
-        ?>
 
         <?php
         include "footer.inc.php";
--- INF1005Project/register.php
+++ INF1005Project/register.php
@@ -68,44 +68,7 @@
             return $data;
         }
         ?>
-        <main class="container">
-            <h1>Register as a User</h1>
-            <p>
-                If you are already registered,
-                <a href="/login.php">Click here </a>to Sign in!!!.
-            </p>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-                <div class="form-group">
-                    <label for="fname">Username:</label>
-                    <input class="form-control"type="text" id="username"
-                           name="username" maxlength="45" placeholder="Enter username"value="<?php echo $username; ?>">
-                    <span class="error"><?php if (isset($nameErr)) echo $nameErr; ?></span>
-                </div>
-                <div class="form-group">
-                    <label for="email">Email:</label>
-                    <input  class="form-control" type="email" id="email"
-                            required name="email" name="email" placeholder="Enter email" value="<?php echo $email; ?>">
-                    <span class="error"><?php if (isset($emailErr)) echo $emailErr; ?></span>
-                </div>
-                <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input  class="form-control" type="password" id="pwd"
-                            required name="pwd" name="pwd" placeholder="Enter password" value="<?php echo $pwd; ?>">
-                    <span class="error"><?php if (isset($passwordErr)) echo $passwordErr; ?></span>
-                </div>
-                <div class="form-group">
-                    <label for="pwd_confirm">Confirm Password:</label>
-                    <input  class="form-control" type="password" id="pwd_confirm"
-                            required name="pwd_confirm" name="pwd_confirm" placeholder="Confirm password" value="<?php echo $pwd_confirm; ?>">
-                    <span class="error"><?php if (isset($pwd_confirmErr)) echo $pwd_confirmErr; ?></span>
-                </div>
-                <div class="form-group">
-                    <button class="btn btn-primary" name="submit" type="submit">Submit</button>
-                </div>
-            </form>
-        </main>
-
-            <?php
+          <?php
             ini_set('display_errors', 1);
             error_reporting(E_ALL);
             // display form data on submission if no errors
@@ -117,7 +80,7 @@
                     saveUserToDB();
                     echo "successfully registered";
                     $message = "succesfully registered";
-                    header("Location: https://35.212.159.197/login.php?message=" . urlencode($message));
+                   
                 }
             }
 
@@ -160,99 +123,46 @@
                     header("Message: succesfully registered");
                 }
             }
-
-//                echo "Connected successfully";
-//                // Prepare query statement:
-//                $stmt = $conn->prepare("INSERT INTO Users (email, username,
-//password,priority) VALUES (?,?,?,?)");
-//                echo "successfully registered 1";
-//                // Bind & execute the query statement:
-//
-//                $stmt->bind_param("ssss", $_POST["email"], $_POST["username"], $pwd_hashed, "3");
-////                echo "<p>" . $stmt . "</p>";
-//
-//                echo "successfully registered 2";
-//            if (!$stmt->execute()) {
-//                $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-//                $success = false;
-//            }
-//            $stmt->close();
-//                $result = $stmt->get_result();
-//            }
-//
-//            // Check connection
-//            if (!$conn) {
-//                die("Connection failed: " . mysqli_connect_error());
-//            } else {
-//            function saveUserToDB2() {
-//                $email = $_POST["email"];
-//                $username = $_POST["username"];
-//
-//                $pwd_hashed = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
-//                $priority = "3";
-//                $success = true;
-//                $errorMsg = "";
-//
-//                $is_active = 1;
-//                $config = parse_ini_file('../private/db-config.ini');
-//                $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
-//
-//// Prepare the SQL statement with placeholders
-//                echo "test 1";
-//                $stmt = $conn->prepare("INSERT INTO Users (email, username,password,priority) VALUES (?,?,?,?)");
-//                echo "test 11";
-//// Bind parameters to the placeholders
-//                $stmt->bind_param("s", $email, $username, $pwd_hashed, $priority);
-//                echo $stmt;
-//                echo "test 2";
-//// Set the parameter values
-//// Execute the prepared statement
-//                $stmt->execute();
-//                echo "test 3";
-//// Close the prepared statement and database connection
-//                $stmt->close();
-//                $conn->close();
-//                echo "successfully registered 4";
-//                echo "test 4";
-//// Redirect the user to a new PHP file
-//                header("Location: http://35.212.159.197/login.php");
-//                header("Message: succesfully registered");
-//                exit();
-//            }
-//                // Create database connection.
-//                $config = parse_ini_file('../private/db-config.ini');
-//                $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
-//
-//                // Check connection
-//                if ($conn->connect_error) {
-//                    $errorMsg = "Connection failed: " . $conn->connect_error;
-//                    $success = false;
-//                }
-////                echo "Connected successfully";
-//                // Prepare query statement:
-//                $stmt = $conn->prepare("INSERT INTO Users (email, username,
-//password,priority) VALUES (?,?,?,?)");
-//                $stmt->bind_param("s", $email, $username, $pwd_hashed, $priority);
-////                echo "<p>" . $stmt . "</p>";
-//                $stmt->execute();
-//
-//                echo $email . $username . $priority;
-//
-//                if (!$stmt->execute()) {
-//                    $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-//                    $success = false;
-//                }
-//                $stmt->close();
-//                $result = $stmt->get_result();
-//                echo $result;
-//            }
-//
-//            // Check connection
-//            if (!$conn) {
-//                die("Connection failed: " . mysqli_connect_error());
-//            } else {
-//                $conn->close();
-//            }
             ?>
+        <main class="container">
+            <h1>Register as a User</h1>
+            <p>
+                If you are already registered,
+                <a href="/login.php">Click here </a>to Sign in!!!.
+            </p>
+            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                <div class="form-group">
+                    <label for="fname">Username:</label>
+                    <input class="form-control"type="text" id="username"
+                           name="username" maxlength="45" placeholder="Enter username"value="<?php echo $username; ?>">
+                    <span class="error"><?php if (isset($nameErr)) echo $nameErr; ?></span>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input  class="form-control" type="email" id="email"
+                            required name="email" name="email" placeholder="Enter email" value="<?php echo $email; ?>">
+                    <span class="error"><?php if (isset($emailErr)) echo $emailErr; ?></span>
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Password:</label>
+                    <input  class="form-control" type="password" id="pwd"
+                            required name="pwd" name="pwd" placeholder="Enter password" value="<?php echo $pwd; ?>">
+                    <span class="error"><?php if (isset($passwordErr)) echo $passwordErr; ?></span>
+                </div>
+                <div class="form-group">
+                    <label for="pwd_confirm">Confirm Password:</label>
+                    <input  class="form-control" type="password" id="pwd_confirm"
+                            required name="pwd_confirm" name="pwd_confirm" placeholder="Confirm password" value="<?php echo $pwd_confirm; ?>">
+                    <span class="error"><?php if (isset($pwd_confirmErr)) echo $pwd_confirmErr; ?></span>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-primary" name="submit" type="submit">Submit</button>
+                </div>
+            </form>
         </main>
+
+          
+
+
+ 
         <?php
