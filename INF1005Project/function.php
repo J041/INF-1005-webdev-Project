986b306f7c288022cff6acf6aa7d24baf7f56f61
J041 <46340016+J041@users.noreply.github.com>
2023-03-17 21:57:23 +0800

    added logout and update user function

--- INF1005Project/function.php
+++ INF1005Project/function.php
@@ -29,4 +29,97 @@ function sanitize_regex_input($data) {
     }
 }
 
+function logout(){
+    session_unset();
+    session_destroy();
+    $_SESSION = array();
+    // if (isset($_COOKIE[session_name()])) {
+    //     setcookie(session_name(), '', time() - 3600, '/');
+    //     header('Location: logout.php');
+    // }
+}
+
+function UpdateUser($new_username, $new_password)
+{
+    mysqli_error(MYSQLI_ERROR_OFF);
+    ini_set("display_errors",1);
+    error_reporting(E_ALL);
+    
+    session_start();
+    // Create database connection.
+    $config = parse_ini_file('../private/db-config.ini');
+    $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+    $returnval = array();
+
+    // Check connection
+    if ($conn->connect_error)
+    {
+        $errorMsg = "Connection failed: " . $conn->connect_error;
+        $success = false;
+    }
+    else
+    {       
+        $errorMsg = "Connection succeed";
+        $updatedusername = false;
+        $updatedpassword = false;
+        $success = false;
+        $email = $_SESSION['email'];
+
+        if (!empty($new_username)) {
+            $stmt = $conn->prepare("UPDATE Users SET username=? where email = ?");
+            $sanitize_username = sanitize_input($new_username);
+            $stmt->bind_param("ss", $sanitize_username, $email);
+            if (!$stmt->execute()) {
+                $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+                $success = false;
+            } else {
+                $result = $stmt->get_result();
+                $_SESSION['username'] = $sanitize_username;
+                $updatedusername = true;
+                $success = true;
+            }
+            $stmt->close();
+        }
+        
+        
+        if (!empty($new_password)) {
+            $stmt = $conn->prepare("UPDATE Users SET password=? where email = ?");
+            $pwd_hashed = password_hash($new_password, PASSWORD_DEFAULT);
+            $stmt->bind_param("ss", $pwd_hashed, $email);
+            // $stmt->bind_param("ss", $_POST["pwd"]);
+            if (!$stmt->execute()) {
+                $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+                $success = false;
+            } else {
+                $result = $stmt->get_result();
+                $success = true;
+                $updatedpassword = true;
+            }
+            $stmt->close();
+        }
+
+        if ($updatedusername && $updatedpassword){
+            $errorMsg = "Update Successful! Username and Password has been updated";
+        } elseif ($updatedusername && !$updatedpassword){
+            $errorMsg = "Update successful!. Username has been updated";
+        } elseif (!$updatedusername && $updatedpassword){
+            $errorMsg = "Update Successful!. Password has been updated";
+        }
+    }
+    $conn->close();
+
+    $returnval['errorMsg'] = $errorMsg;
+    $returnval['updatedpassword'] = $updatedpassword;
+
+    // array_push( $returnval, $errorMsg );
+    // array_push( $returnval, $updatedpassword );
+
+    if ($success){
+        return $returnval;
+    } else {
+        return False;
+    }
+
+}
+
 ?>
