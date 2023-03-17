046265b4b67036fb560afeaca8b3b48346d2da07
J041 <46340016+J041@users.noreply.github.com>
2023-03-18 03:48:59 +0800

    added authentication check

--- INF1005Project/updateprofile.php
+++ INF1005Project/updateprofile.php
@@ -12,44 +12,47 @@
 
         <?php 
             require 'function.php';
-            echo $_SERVER['REQUEST_METHOD'];
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $returnval = UpdateUser($_POST["username"], $_POST["pwd"]);
-                if ($returnval != False){
-                    $Msg = $returnval['errorMsg'];
-                    $updatedpassword = $returnval['updatedpassword'];
-                    $success = True;
-                } else {
-                    $success = False;
+                if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
+                    $returnval = UpdateUser($_POST["username"], $_POST["pwd"]);
+                    if ($returnval != False){
+                        $Msg = $returnval['errorMsg'];
+                        $updatedpassword = $returnval['updatedpassword'];
+                        $success = True;
+                    } else {
+                        $success = False;
+                    }
                 }
             }
         ?>
 
         <div class="container">
 
-            <!-- <form action="process_updateprofile.php" method="post" enctype="multipart/form-data"> -->
-            <form action="updateprofile.php" method="post">
-                <div class="form-group">
-                    <label for="email">Username:</label>
-                    <input class="form-control" type="username" id="username"
-                    name="username" placeholder="Enter username">
-                </div>
-                <div class="form-group">
-                    <label for="pwd">Password:</label>
-                    <input class="form-control" type="password" id="pwd"
-                    name="pwd" placeholder="Enter password">
-                </div>
-                <!-- <div class="form-group">
-                    <label for="profilepic">Profile Picture</label>
-                    <br>
-                    <input type="file" id="profilepic" 
-                           name="profilepic">
-                </div> -->
-                <div class="form-group">
-                        <button class="btn btn-primary" type="submit">Update</button>
-                </div>
-            </form>
             <?php
+            if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
+                echo '<!-- <form action="process_updateprofile.php" method="post" enctype="multipart/form-data"> -->
+                <form action="updateprofile.php" method="post">
+                    <div class="form-group">
+                        <label for="email">Username:</label>
+                        <input class="form-control" type="username" id="username"
+                        name="username" placeholder="Enter username">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd">Password:</label>
+                        <input class="form-control" type="password" id="pwd"
+                        name="pwd" placeholder="Enter password">
+                    </div>
+                    <!-- <div class="form-group">
+                        <label for="profilepic">Profile Picture</label>
+                        <br>
+                        <input type="file" id="profilepic" 
+                            name="profilepic">
+                    </div> -->
+                    <div class="form-group">
+                            <button class="btn btn-primary" type="submit">Update</button>
+                    </div>
+                </form>';
+                
                 if (isset($Msg)){
                     if ($success){
                         if ($updatedpassword){
@@ -76,6 +79,11 @@
                         echo '<div class="alert alert-danger" role="alert"><p>'. $Msg . '</p></div>';
                     }
                 }
+            } else {
+                echo    '<div class="alert alert-danger" role="alert">'.
+                            '<p>You must be logged in to access this page</p>' . 
+                        '</div>';
+            }
             ?> 
         </div>
         <?php
