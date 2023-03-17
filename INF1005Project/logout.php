7a4ce5ecff8fd2b7df4f8df54fde976265d41a0f
J041 <46340016+J041@users.noreply.github.com>
2023-03-17 21:56:39 +0800

    removed logout function to function.php

--- INF1005Project/logout.php
+++ INF1005Project/logout.php
@@ -6,39 +6,22 @@
         ?>
     </head>
     <?php
-    include "nav.inc.php";
-    session_start();
-    
-    // if (isset($_POST['submit'])) {
-    //     logout();
-    // }
-    logout();
-    
-    function logout(){
-        session_unset();
-        session_destroy();
-        $_SESSION = array();
-        if (isset($_COOKIE[session_name()])) {
-            setcookie(session_name(), '', time() - 3600, '/');
-            header('Location: logout.php');
-        }
-    }
-    
-    // foreach ($_SESSION as $key => $value) {
-    //     echo $key . ' => ' . $value . '<br>';
-    // }
+        include "nav.inc.php";
+    ?>
+    <?php
+        session_start();
+        require 'function.php';
+        logout();
     ?>
     <body>
         <main class="container">
             <h1>You have successfully Logged out</h1>
             <p>
                 <a href="login.php">click here to return to login page</a>
-                
             </p>
-         
         </main>
         <?php
-        include "footer.inc.php";
+            include "footer.inc.php";
         ?>
     </body>
 
