011bdd854a21b59e38aa8490f07255771db3fa35
shawnoon <shawnoon123@gmail.com>
2023-03-20 22:00:02 +0800

    register error messages fixed

--- INF1005Project/register.php
+++ INF1005Project/register.php
@@ -10,10 +10,10 @@
     ?>
     <body>
         <?php
-        global $email, $username, $pwd_hashed;
-        
+        global $email, $username, $pwd_hashed, $usernameErr, $emailErr, $passwordErr, $pwd_confirmErr;
+
         // define variables and set to empty values
-        $usernameErr = $emailErr = $passwordErr = $pwd_confirmErr = "";
+        $usernameErr = $emailErr = $passwordErr = $pwd_confirmErr = null;
         $username = $email = $pwd = $pwd_confirm = "";
 
         if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -38,12 +38,12 @@
             }
 
             if (empty($_POST["pwd"])) {
-                $passwordErr .= "password is required.<br>";
+                $passwordErr = "password is required.<br>";
 
                 $success = false;
             } else {
                 if ($_POST["pwd"] != $_POST["pwd_confirm"]) {
-                    $pwd_confirmErr .= "password dont match with confirm pass";
+                    $pwd_confirmErr = "password dont match with confirm pass";
                     $success = false;
                 }
             }
@@ -69,118 +69,122 @@
             return $data;
         }
         ?>
-          <?php
-            ini_set('display_errors', 1);
-            error_reporting(E_ALL);
-            // display form data on submission if no errors
-            if (isset($_POST['submit'])) {
-                echo "pre test2" . '<br>';
-                echo $usernameErr . '<br>';
-                echo $emailErr . '<br>';
-                echo $passwordErr . '<br>';
-                echo $pwd_confirmErr . '<br>';
-                if (empty($usernameErr) && empty($emailErr) && empty($passwordErr) && empty($pwd_confirmErr)) {
+        <?php
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+        // display form data on submission if no errors
+        if (isset($_POST['submit'])) {
+            if (empty($usernameErr) && empty($emailErr) && empty($passwordErr) && empty($pwd_confirmErr)) {
 
-                    echo "pre test";
-                    saveUserToDB();
-                    echo "successfully registered";
-                    $message = "succesfully registered";
-                   
-                }
+                echo "pre test";
+                saveUserToDB();
+                echo "successfully registered";
+                $message = "succesfully registered";
             }
+        }
 
-            function saveUserToDB() {
-                global $pwd_hashed, $priority, $errorMsg, $success;
-                $pwd_hashed = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
-                $priority = 3;
-                $success = true;
-                $errorMsg = "";
+        function saveUserToDB() {
+            global $pwd_hashed, $priority, $errorMsg, $success;
+            $pwd_hashed = password_hash($_POST["pwd"], PASSWORD_DEFAULT);
+            $priority = 3;
+            $success = true;
+            $errorMsg = "";
 
-                // Create database connection.
-                $config = parse_ini_file('../private/db-config.ini');
-                $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
-                echo "test1";
+            // Create database connection.
+            $config = parse_ini_file('../private/db-config.ini');
+            $conn = new mysqli($config['servername'], $config['username'], $config['password'], $config['dbname']);
+            echo "test1";
 
-                // Check connection
-                if ($conn->connect_error) {
-                    $errorMsg = "Connection failed: " . $conn->connect_error;
+            // Check connection
+            if ($conn->connect_error) {
+                $errorMsg = "Connection failed: " . $conn->connect_error;
+                $success = false;
+            } else {
+//                    echo "test2";let
+                $stmt = $conn->prepare("INSERT INTO Users (email, username,password,priority) VALUES (?,?,?,?);");
+                $stmt->bind_param("ssss", $_POST["email"], $_POST["username"], $pwd_hashed, $priority);
+                echo "test3";
+                echo $priority . ($_POST["email"]);
+                $stmt->execute();
+                echo "test4";
+                if (!$stmt->execute()) {
+                    $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
                     $success = false;
                 } else {
-//                    echo "test2";let
-                    $stmt = $conn->prepare("INSERT INTO Users (email, username,password,priority) VALUES (?,?,?,?);");
-                    $stmt->bind_param("ssss", $_POST["email"], $_POST["username"], $pwd_hashed, $priority);
-                    echo "test3";
-                    echo $priority . ($_POST["email"]);
-                    $stmt->execute();
-                    echo "test4";
-                    if (!$stmt->execute()) {
-                        $errorMsg = "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-                        $success = false;
-                    } else {
-                        echo "test5";
-                        $result = $stmt->get_result();
+                    echo "test5";
+                    $result = $stmt->get_result();
 
-                        $success = true;
-                    }
-                    $stmt->close();
-
-                    $stmt2 = $conn->prepare("INSERT INTO Order_History (Users_email, purchased) VALUES (?, ?)");
-                    $purchased = 0;
-                    $stmt2->bind_param("si", $_POST["email"], $purchased);
-                    if (!$stmt2->execute()) {
-                        $errorMsg = "Execute failed: (" . $stmt2->errno . ") " . $stmt2->error;
-                        $success = false;
-                    } else {
-                        $success = true;
-                    }
-                    $stmt2->close();
-
-                    $conn->close();
-                    echo '<script>window.location.href = "login.php";</script>';
-
-                    //header("Message: succesfully registered");
+                    $success = true;
                 }
+                $stmt->close();
+
+                $stmt2 = $conn->prepare("INSERT INTO Order_History (Users_email, purchased) VALUES (?, ?)");
+                $purchased = 0;
+                $stmt2->bind_param("si", $_POST["email"], $purchased);
+                if (!$stmt2->execute()) {
+                    $errorMsg = "Execute failed: (" . $stmt2->errno . ") " . $stmt2->error;
+                    $success = false;
+                } else {
+                    $success = true;
+                }
+                $stmt2->close();
+
+                $conn->close();
+                echo '<script>window.location.href = "login.php";</script>';
+
+                //header("Message: succesfully registered");
             }
-            ?>
-        <main class="container">
+        }
+
+        echo'  <main class="container">
             <h1>Register as a User</h1>
             <p>
                 If you are already registered,
                 <a href="/login.php">Click here to Sign in !</a>.
             </p>
-            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+            <form method="post" action="register.php">
                 <div class="form-group">
                     <label for="fname">Username:</label>
                     <input class="form-control"type="text" id="username"
-                           name="username" maxlength="45" placeholder="Enter username"value="<?php echo $username; ?>">
-                    <span class="error"><?php if (isset($nameErr)) echo $usernameErr; ?></span>
+                           required name="username" maxlength="45" placeholder="Enter username">
                 </div>
                 <div class="form-group">
                     <label for="email">Email:</label>
                     <input  class="form-control" type="email" id="email"
-                            required name="email" name="email" placeholder="Enter email" value="<?php echo $email; ?>">
-                    <span class="error"><?php if (isset($emailErr)) echo $emailErr; ?></span>
+                            required name="email" placeholder="Enter email">
                 </div>
                 <div class="form-group">
                     <label for="pwd">Password:</label>
                     <input  class="form-control" type="password" id="pwd"
-                            required name="pwd" name="pwd" placeholder="Enter password" value="<?php echo $pwd; ?>">
-                    <span class="error"><?php if (isset($passwordErr)) echo $passwordErr; ?></span>
+                            required name="pwd" placeholder="Enter password">
                 </div>
                 <div class="form-group">
                     <label for="pwd_confirm">Confirm Password:</label>
                     <input  class="form-control" type="password" id="pwd_confirm"
-                            required name="pwd_confirm" name="pwd_confirm" placeholder="Confirm password" value="<?php echo $pwd_confirm; ?>">
-                    <span class="error"><?php if (isset($pwd_confirmErr)) echo $pwd_confirmErr; ?></span>
+                            required name="pwd_confirm" placeholder="Confirm password">
                 </div>
                 <div class="form-group">
                     <button class="btn btn-primary" name="submit" type="submit">Submit</button>
                 </div>
             </form>
-        </main>
+        </main>';
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-          
-
-
- 
+            if ($usernameErr !==null) {
+                echo '<div class="alert alert-warning" role="alert"><p>' . $usernameErr . '</p></div>';
+            } elseif ($emailErr!==null) {
+                echo '<div class="alert alert-warning" role="alert"><p>' . $emailErr . '</p></div>';
+            } elseif ($passwordErr!==null) {
+                echo '<div class="alert alert-warning" role="alert"><p>' . $passwordErr . '</p></div>';
+            } elseif ($pwd_confirmErr!==null) {
+                echo '<div class="alert alert-warning" role="alert"><p>]' . $pwd_confirmErr . '</p></div>';
+            } else {
+                
+            }
+        }
+        ?>
         <?php
+        include "footer.inc.php";
+        ?>
+    </body>
+</html>
