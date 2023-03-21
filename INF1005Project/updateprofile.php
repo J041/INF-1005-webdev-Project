e1b26c49730e5ff19fdbccd68b4e370f7d1ba2a9
J041 <46340016+J041@users.noreply.github.com>
2023-03-22 02:36:09 +0800

    Update updateprofile.php
    cleanup

--- INF1005Project/updateprofile.php
+++ INF1005Project/updateprofile.php
@@ -25,14 +25,6 @@
                             $success = False;
                         }
                     }
-                    // if ($returnval['success'] != False){
-                    //     $Msg = $returnval['errorMsg'];
-                    //     $updatedpassword = $returnval['updatedpassword'];
-                    //     $success = True;
-                    // } else {
-                    //     $Msg = $returnval['errorMsg'];
-                    //     $success = False;
-                    // }
                 }
             }
         ?>
@@ -41,8 +33,7 @@
 
             <?php
             if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
-                echo '<!-- <form action="process_updateprofile.php" method="post" enctype="multipart/form-data"> -->
-                <form action="updateprofile.php" method="post">
+                echo '<form action="updateprofile.php" method="post">
                     <div class="form-group">
                         <label for="email">Username:</label>
                         <input class="form-control" type="username" id="username"
@@ -58,22 +49,12 @@
                         <input class="form-control" type="password" id="new_pwd"
                         name="new_pwd" placeholder="Enter password">
                     </div>
-                    <!-- <div class="form-group">
-                        <label for="profilepic">Profile Picture</label>
-                        <br>
-                        <input type="file" id="profilepic" 
-                            name="profilepic">
-                    </div> -->
                     <div class="form-group">
                             <button class="btn btn-primary" type="submit">Update</button>
                     </div>
                 </form>';
                 
-                // if (isset($Msg)){
-                    // echo "success is:" . $success;
-                    // echo "success isset is:" . isset($success);
                 if (isset($success) && $success){
-                    // echo "triggered";
                     if ($updatedusername == 1 && $updatedpassword == 1){
                         $Msg = "Update Successful! Username and Password has been updated";
                         echo    '<div class="alert alert-success" role="alert">'.
@@ -90,21 +71,7 @@
                                     setTimeout(logoutandlogin, 5000);
                                 </script>
                             ';
-                    } else {
-
-                        // if ($updatedusername == 1 && $updatedpassword != 1){
-                        //     $Msg = "Username has been updated";
-                        //     echo    '<div class="alert alert-success" role="alert">'.
-                        //                 '<p>'. $Msg . '</p>' . 
-                        //             '</div>';
-                        //     $Msg = "Username has been updated";
-                        //     echo    '<div class="alert alert-success" role="alert">'.
-                        //                 '<p>'. $Msg . '</p>' . 
-                        //             '</div>';
-                        // } else {
-                            
-                        // }
-                        
+                    } else {                   
 
                         if ($updatedusername == 1 && $updatedpassword == 3){
                             $Msg = "Update successful !. Username has been updated";
@@ -168,77 +135,8 @@
                                 }
                             }
                         }
-            
-
-                        // if ($updatedpassword != 3){
-                        //     if ($updatedpassword == 1){
-                        //         $Msg = "Update Successful!. Password has been updated";
-                        //         echo    '<div class="alert alert-success" role="alert">'.
-                        //                     '<p>'. $Msg . '</p>' . 
-                        //                     '<p> <b>Please relogin</b> You will be redirected after 5 seconds </p>' . 
-                        //                 '</div>';
-                        //         echo '
-                        //             <script>
-                        //                 function logoutandlogin(){
-                        //                     fetch("logout.php")
-                        //                     window.location.href="login.php"
-                        //                 }
-    
-                        //                 setTimeout(logoutandlogin, 5000);
-                        //             </script>
-                        //             ';
-                        //     }
-                
-                        //     if ($updatedpassword == 0){
-                        //         $Msg = "Update Failed!. Current password is Incorrect";
-                        //         echo '<div class="alert alert-danger" role="alert"><p>'. $Msg . '</p></div>';
-                        //     } 
-                
-                        //     if ($updatedpassword == 2){
-                        //         $Msg = "Update Failed!. Current password is required to update password";
-                        //         echo '<div class="alert alert-danger" role="alert"><p>'. $Msg . '</p></div>';
-                        //     }
-                        // }
                     }
-
-                    // if ($updatedpassword == 0 || $updatedpassword == 2){
-                    //     $Msg = $returnval['errorMsg'];
-                    //     $success = False;
-                    // } else {
-                    //     $Msg = $returnval['errorMsg'];
-                    //     $success = True;
-                    // }
-
-
-
-                    // if ($updatedpassword){
-                    //     echo    '<div class="alert alert-success" role="alert">'.
-                    //                 '<p>'. $Msg . '</p>' . 
-                    //                 '<p> <b>Please relogin</b> You will be redirected after 5 seconds </p>' . 
-                    //             '</div>';
-                    //     echo '
-                    //         <script>
-                    //             function logoutandlogin(){
-                    //                 fetch("logout.php")
-                    //                 window.location.href="login.php"
-                    //             }
-
-                    //             setTimeout(logoutandlogin, 5000);
-                    //         </script>
-                    //         ';
-                    // } else {
-                    //     echo    '<div class="alert alert-success" role="alert">'.
-                    //                 '<p>'. $Msg . '</p>' . 
-                    //             '</div>';
-                    // }
-
-
-
                 }
-                // } else {
-                //     echo '<div class="alert alert-danger" role="alert"><p>'. $Msg . '</p></div>';
-                // }
-                // }
             } else {
                 echo    '<div class="alert alert-danger" role="alert">'.
                             '<p>You must be logged in to access this page</p>' . 
