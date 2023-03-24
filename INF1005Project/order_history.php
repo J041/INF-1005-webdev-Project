2761074e6a757962cbc8d23ed51617921f5bae26
J041 <46340016+J041@users.noreply.github.com>
2023-03-24 21:26:34 +0800

    Update order_history.php
    added access control

--- INF1005Project/order_history.php
+++ INF1005Project/order_history.php
@@ -13,20 +13,20 @@
         <?php
         /*
 
-          {
-          orderhistory_order_id : {
-          cartitem_productid : {
-          product_name: xyz,
-          quantity: 123,
-          price: 789
-          },
-          cartitem_productid : {
-          product_name: xyz,
-          quantity: 123,
-          price: 789
-          }, ...
-          }, ...
-          }
+        {
+        orderhistory_order_id : {
+            cartitem_productid : {
+                product_name: xyz,
+                quantity: 123,
+                price: 789
+            },
+            cartitem_productid : {
+                    product_name: xyz,
+                    quantity: 123,
+                    price: 789
+                }, ...
+            }, ...
+        }
 
          */
 
@@ -108,170 +108,188 @@
         }
         $conn->close();
         ?>
-        <div class="order-history container-fluid">
-            <div class="order-history-header row">
-                <div class="col-lg-12 col-xl-12">
-                    <h1>Order History</h1>
-                </div>
-            </div>
 
-            <?php
-            if (isset($_SESSION['priority']) && !empty($_SESSION['priority'])) {
-                if (($_SESSION['priority']) == 3) {
+           <?php
 
-                    $html_output .= '
+            if(isset($_SESSION['username']) && !empty($_SESSION['username'])){
+                if (isset($_SESSION['priority']) && !empty($_SESSION['priority']) && $_SESSION['priority'] != 2) {
+                    $html_output = '
+                    <div class="order-history container-fluid">
+                    <div class="order-history-header row">
+                        <div class="col-lg-12 col-xl-12">
+                            <h1>Order History</h1>
+                        </div>
+                    </div>
+                    ';
 
-                <div class="order-history-data row">              
-                    <table class="table table-striped table-hover table-responsive-xl">
-                        <thead class="thead-light">
-                            <tr>
-                                <td scope="col">Order</td>
-                                <td scope="col">Total Price</td>
-                                <td scope="col"></td>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            ';
-
-                    // Output Products into HTML Table
-                    $total_prices = [];
-                    foreach ($order_ids as $order_id => $value) {
-                        $total_price = 0;
-                        $html_output .= "<tr>";
-                        $html_output .= "<td scope=\"row\">" . sprintf("%08d", $order_id) . "</td>";
-
-                        foreach ($value as $sub_key => $sub_val) {
-                            $total_price += $sub_val["total"];
+                    if (($_SESSION['priority']) == 3) {
+    
+                        $html_output .= '
+    
+                    <div class="order-history-data row">              
+                        <table class="table table-striped table-hover table-responsive-xl">
+                            <thead class="thead-light">
+                                <tr>
+                                    <td scope="col">Order</td>
+                                    <td scope="col">Total Price</td>
+                                    <td scope="col"></td>
+                                </tr>
+                            </thead>
+                            <tbody>
+    
+                                ';
+    
+                        // Output Products into HTML Table
+                        $total_prices = [];
+                        foreach ($order_ids as $order_id => $value) {
+                            $total_price = 0;
+                            $html_output .= "<tr>";
+                            $html_output .= "<td scope=\"row\">" . sprintf("%08d", $order_id) . "</td>";
+    
+                            foreach ($value as $sub_key => $sub_val) {
+                                $total_price += $sub_val["total"];
+                            }
+                            array_push($total_prices, number_format($total_price, 2, '.', ''));
+                            $html_output .= "<td scope=\"row\">SGD $" . number_format($total_price, 2, '.', '') . "</td>";
+                            $html_output .= "<td>"
+                                    . "<button type=\"button\" class=\"btn btn-outline-info btn-sm\" data-toggle=\"modal\" data-target=\"#order_details_item_" . $order_id . "\">Details</button>"
+                                    . "</td>"
+                                    . "</tr>";
                         }
-                        array_push($total_prices, number_format($total_price, 2, '.', ''));
-                        $html_output .= "<td scope=\"row\">SGD $" . number_format($total_price, 2, '.', '') . "</td>";
-                        $html_output .= "<td>"
-                                . "<button type=\"button\" class=\"btn btn-outline-info btn-sm\" data-toggle=\"modal\" data-target=\"#order_details_item_" . $order_id . "\">Details</button>"
-                                . "</td>"
-                                . "</tr>";
-                    }
-
-                    $html_output .= '
+    
+                        $html_output .= '
+                            </tbody>
+                            </table>
+                        </div>
+                                ';
+                    } else {
+                        $html_output .= '
+                    <div class="order-history-data row">              
+                        <table class="table table-striped table-hover table-responsive-xl">
+                            <thead class="thead-light">
+                                <tr>
+                                    <td scope="col">Email</td>
+                                    <td scope="col">Order</td>
+                                    <td scope="col">Total Price</td>
+                                    <td scope="col"></td>
+                                </tr>
+                            </thead>
+                            <tbody>
+    
+                                ';
+    
+                        // Output Order History into HTML Table
+                        $total_prices = [];
+                        foreach ($order_ids as $order_id => $value) {
+                            $total_price = 0;
+                            $html_output .= "<tr>";
+                            for ($i = 0; $i < sizeof($paid_order_history); $i++) {
+                                if ($paid_order_history[$i][0] == $order_id) {
+                                    $html_output .= "<td scope=\"row\">" . $paid_order_history[$i][1] . "</td>";
+                                    break;
+                                }
+                            }
+                            $html_output .= "<td scope=\"row\">" . sprintf("%08d", $order_id) . "</td>";
+    
+                            foreach ($value as $sub_key => $sub_val) {
+                                $total_price += $sub_val["total"];
+                            }
+                            array_push($total_prices, number_format($total_price, 2, '.', ''));
+                            $html_output .= "<td scope=\"row\">SGD $" . number_format($total_price, 2, '.', '') . "</td>";
+                            $html_output .= "<td>"
+                                    . "<button type=\"button\" class=\"btn btn-outline-info btn-sm\" data-toggle=\"modal\" data-target=\"#order_details_item_" . $order_id . "\">Details</button>"
+                                    . "</td>"
+                                    . "</tr>";
+                        }
+    
+                        $html_output .= '
                         </tbody>
                         </table>
                     </div>
-                            ';
-                } else {
-                    $html_output .= '
-                <div class="order-history-data row">              
-                    <table class="table table-striped table-hover table-responsive-xl">
-                        <thead class="thead-light">
-                            <tr>
-                                <td scope="col">Email</td>
-                                <td scope="col">Order</td>
-                                <td scope="col">Total Price</td>
-                                <td scope="col"></td>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            ';
-
-                    // Output Order History into HTML Table
-                    $total_prices = [];
+                                ';
+                    }
+    
                     foreach ($order_ids as $order_id => $value) {
                         $total_price = 0;
-                        $html_output .= "<tr>";
-                        for ($i = 0; $i < sizeof($paid_order_history); $i++) {
-                            if ($paid_order_history[$i][0] == $order_id) {
-                                $html_output .= "<td scope=\"row\">" . $paid_order_history[$i][1] . "</td>";
-                                break;
-                            }
-                        }
-                        $html_output .= "<td scope=\"row\">" . sprintf("%08d", $order_id) . "</td>";
-
-                        foreach ($value as $sub_key => $sub_val) {
-                            $total_price += $sub_val["total"];
-                        }
-                        array_push($total_prices, number_format($total_price, 2, '.', ''));
-                        $html_output .= "<td scope=\"row\">SGD $" . number_format($total_price, 2, '.', '') . "</td>";
-                        $html_output .= "<td>"
-                                . "<button type=\"button\" class=\"btn btn-outline-info btn-sm\" data-toggle=\"modal\" data-target=\"#order_details_item_" . $order_id . "\">Details</button>"
-                                . "</td>"
-                                . "</tr>";
-                    }
-
-                    $html_output .= '
-                    </tbody>
-                    </table>
-                </div>
-                            ';
-                }
-
-                foreach ($order_ids as $order_id => $value) {
-                    $total_price = 0;
-                    $html_output .= "<div aria-hidden=\"true\" aria-labelledby=\"order_details_item_" . $order_id . "\" class=\"order-history-item modal fade\" id=\"order_details_item_" . $order_id . "\" role=\"dialog\" tabindex=\"-1\">";
-
-                    $html_output .= '
-    <div class="modal-dialog modal-xl modal-dialog-scrollable" role="document">
-        <div class="modal-content">
-            <div class="modal-body">
-                <div class="container-fluid">
-                    <div class="order-history-item-btn row">
-                        <button data-dismiss="modal" type="button"><i class="fa-solid fa-xmark"></i></button>
-                    </div>
-                    <div class="order-history-data-details row">
-                            ';
-                    
-                    $html_output .= "<h1>Order ID: #". sprintf("%08d", $order_id) ."</h1>";
-                    
-                    $html_output .= '
-                    </div>
-                    <div class="order-history-data-details row">              
-                        <table class="table table-hover table-responsive-xl">
-                            <thead class="thead-light">
-                            ';
-                    
-                    for ($i = 0; $i < sizeof($paid_order_history); $i++) {
-                        if ($paid_order_history[$i][0] == $order_id) {
-                            $html_output .= "<tr>";
-                            $html_output .= "<th scope=\"col\" colspan=\"2\">Order At: " . $paid_order_history[$i][2] . "</th>";
-                            $html_output .= "<th scope=\"col\" colspan=\"2\">Sub-Total: SGD $" . $total_prices[$i] . "</th>";
-                            $html_output .= "<tr>";
-                        }
-                    }
-
-                    $html_output .= '
-                                        <tr>
-                                            <td scope="col">Product Name</td>
-                                            <td scope="col">Quantity</td>
-                                            <td scope="col">Price</td>
-                                            <td scope="col">Total</td>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                            ';
-
-                    foreach ($value as $sub_key => $sub_val) {
+                        $html_output .= "<div aria-hidden=\"true\" aria-labelledby=\"order_details_item_" . $order_id . "\" class=\"order-history-item modal fade\" id=\"order_details_item_" . $order_id . "\" role=\"dialog\" tabindex=\"-1\">";
+    
+                        $html_output .= '
+                        <div class="modal-dialog modal-xl modal-dialog-scrollable" role="document">
+                            <div class="modal-content">
+                                <div class="modal-body">
+                                    <div class="container-fluid">
+                                        <div class="order-history-item-btn row">
+                                            <button data-dismiss="modal" type="button"><i class="fa-solid fa-xmark"></i></button>
+                                        </div>
+                        <div class="order-history-data-details row">
+                                ';
+                        
+                        $html_output .= "<h1>Order ID: #". sprintf("%08d", $order_id) ."</h1>";
+                        
+                        $html_output .= '
+                        </div>
+                        <div class="order-history-data-details row">              
+                            <table class="table table-hover table-responsive-xl">
+                                <thead class="thead-light">
+                                ';
+                        
                         for ($i = 0; $i < sizeof($paid_order_history); $i++) {
                             if ($paid_order_history[$i][0] == $order_id) {
                                 $html_output .= "<tr>";
-                                $html_output .= "<td scope=\"row\">" . $sub_val["product_name"] . "</td>";
-                                $html_output .= "<td scope=\"row\">" . $sub_val["quantity"] . "</td>";
-                                $html_output .= "<td scope=\"row\">SGD $" . number_format($sub_val["price"], 2, '.', '') . "</td>";
-                                $html_output .= "<td scope=\"row\">SGD $" . number_format($sub_val["total"], 2, '.', '') . "</td>";
-                                $html_output .= "</tr>";
+                                $html_output .= "<th scope=\"col\" colspan=\"2\">Order At: " . $paid_order_history[$i][2] . "</th>";
+                                $html_output .= "<th scope=\"col\" colspan=\"2\">Sub-Total: SGD $" . $total_prices[$i] . "</th>";
+                                $html_output .= "<tr>";
                             }
                         }
+    
+                        $html_output .= '
+                                            <tr>
+                                                <td scope="col">Product Name</td>
+                                                <td scope="col">Quantity</td>
+                                                <td scope="col">Price</td>
+                                                <td scope="col">Total</td>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                ';
+    
+                        foreach ($value as $sub_key => $sub_val) {
+                            for ($i = 0; $i < sizeof($paid_order_history); $i++) {
+                                if ($paid_order_history[$i][0] == $order_id) {
+                                    $html_output .= "<tr>";
+                                    $html_output .= "<td scope=\"row\">" . $sub_val["product_name"] . "</td>";
+                                    $html_output .= "<td scope=\"row\">" . $sub_val["quantity"] . "</td>";
+                                    $html_output .= "<td scope=\"row\">SGD $" . number_format($sub_val["price"], 2, '.', '') . "</td>";
+                                    $html_output .= "<td scope=\"row\">SGD $" . number_format($sub_val["total"], 2, '.', '') . "</td>";
+                                    $html_output .= "</tr>";
+                                }
+                            }
+                        }
+                        $html_output .= '
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            </div>
+                                ';
                     }
-                    $html_output .= '
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    </div>
-                            ';
+    
+                    echo $html_output;
                 }
 
+            } else {
+                $html_output = "<div class=\"container-fluid\" role=\"alert\">"
+                        . "<div class=\"row\">"
+                        . "<div class=\"output-msg card\">"
+                        . "<div class=\"card-body\">"
+                        . "<p class=\"text-danger\">You must be logged in to access this page</p>"
+                        . "</div>"
+                        . "</div>"
+                        . "</div>"
+                        . "</div>";
                 echo $html_output;
             }
             ?>
