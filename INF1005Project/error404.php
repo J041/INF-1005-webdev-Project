1a507f1c5820723855e6b9bb2053f0344aec6798
J041 <46340016+J041@users.noreply.github.com>
2023-03-24 21:15:27 +0800

    Create error404.php

---
+++ INF1005Project/error404.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <?php
+        include "header.inc.php";
+        ?>
+    </head>
+    <body>
+        <?php
+        include "nav.inc.php";
+        ?>
+        <div>
+            <div class="container-fluid" role="alert">
+                <div class="row">
+                    <div class="output-msg card">
+                        <div class="card-body">
+                            <p class="text-danger">Oops! Page Not Found.</p>
+                            <p class="text-danger">The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
+                            <a href="/">Go to Homepage</a>
+                            <form action="index.php" method="get">
+                                <button class="btn btn-primary" type="submit">Go to Homepage</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php
+        include "footer.inc.php";
+        ?>
+    </body>
+</html>
